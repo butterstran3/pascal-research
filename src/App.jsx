c94982e355f1f8f8cd4be8359dbe83e058ed35d5
Jimmy Tran <jimmitran3@gmail.com>
2023-02-03 19:53:51 +1100

    Pricing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from '../src/components/nav/Nav';
 import Content from '../src/components/content/Content';
+import Pricing from "./components/pricing/Pricing";
 
 const App = () => {
     return (
         <>
             <Nav />
             <Content />
+            <Pricing />
         </>
     )
 };
--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -3,13 +3,71 @@ import './pricing.css';
 
 const Pricing = () => {
     return (
-        <section id="pricing">
-            <h5>Looking to sign up?</h5>
-            <h2>Pricing</h2>
-
-            <div className="container pricing_container">
-                
+        <section className="container pricing_container" id="pricing">
+            <div>
+                <div className="pricing_title">
+                    <h4>Looking to sign up?</h4>
+                    <h1>Pricing</h1>
+                </div>
+                <div className="pricing_options">
+                    <article className="pricing_item">
+                        <h2>
+                            FREE
+                        </h2>
+                        <h3>
+                            Free
+                        </h3>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                            <li>Elementum tempus egestas sed sed risus pretium quam</li>
+                            <li>Eget velit aliquet sagittis id consectetur purus ut</li>
+                            <li>Nunc lobortis mattis aliquam faucibus purus in</li>
+                            <li>Ut diam quam nulla porttitor</li>
+                        </ul>
+                        <div className="sign-up-btn_container">
+                            <button className="sign-up_btn">Sign Up</button>
+                        </div>
+                    </article>
+                    <article className="pricing_item">
+                        <h2>
+                            Standard
+                        </h2>
+                        <h3>
+                            $50 per month
+                        </h3>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                            <li>Elementum tempus egestas sed sed risus pretium quam</li>
+                            <li>Eget velit aliquet sagittis id consectetur purus ut</li>
+                            <li>Nunc lobortis mattis aliquam faucibus purus in</li>
+                            <li>Ut diam quam nulla porttitor</li>
+                        </ul>
+                        <div className="sign-up-btn_container">
+                            <button className="sign-up_btn">Sign Up</button>
+                        </div>
+                    </article>
+                    <article className="pricing_item">
+                        <h2>
+                            Premium
+                        </h2>
+                        <h3>
+                            $100 per month
+                        </h3>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                            <li>Elementum tempus egestas sed sed risus pretium quam</li>
+                            <li>Eget velit aliquet sagittis id consectetur purus ut</li>
+                            <li>Nunc lobortis mattis aliquam faucibus purus in</li>
+                            <li>Ut diam quam nulla porttitor</li>
+                        </ul>
+                        <div className="sign-up-btn_container">
+                            <button className="sign-up_btn">Sign Up</button>
+                        </div>
+                    </article>
+                </div>
             </div>
         </section>
     )
 }
+
+export default Pricing;
